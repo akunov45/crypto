9c323b81d420ec9b0e2fbf063aa7e8282e54cd02
kutman <frontdevkg@gmail.com>
2022-01-19 20:41:25 +0600

    crypto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Benefits from './components/benefits/benefits';
+import Charts from './components/charts/chart';
 import Header from './components/header';
-import SectionFour from './components/section-four/section-four';
-import SectionTwo from './components/section-two/section-two';
 import Implementation from './components/implementation';
+import Network from './components/network/network';
+import SectionEleven from './components/section-eleven/section-eleven';
+import SectionFour from './components/section-four/section-four';
+import SectionSeven from './components/section-seven/section-seven';
+import SectionTen from './components/section-ten/section-ten';
+import SectionTwo from './components/section-two/section-two';
 
 function App() {
     return (
@@ -13,6 +18,11 @@ function App() {
             <Benefits />
             <SectionFour />
             <Implementation />
+            <Network />
+            <SectionSeven />
+            <Charts />
+            <SectionTen />
+            <SectionEleven />
         </>
     );
 }
--- src/components/implementation/implementation.js
+++ src/components/implementation/implementation.js
@@ -1,6 +1,33 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
 import './implementation.css';
+
+import alpha from '../../images/alpha.svg';
+import architecture from '../../images/architectur.svg';
+import copixel from '../../images/copixel.svg';
+import denvers from '../../images/denvers.svg';
+import remark from '../../images/remark.png';
+import remote from '../../images/remote.png';
+
+const roadImages = [
+    {
+        img: copixel,
+    },
+    {
+        img: remark,
+    },
+    {
+        img: remote,
+    },
+    {
+        img: architecture,
+    },
+    {
+        img: denvers,
+    },
+    {
+        img: alpha,
+    },
+];
 
 export default function Implementation() {
     return (
@@ -24,26 +51,60 @@ export default function Implementation() {
                             holders in the shortest time possible.
                         </p>
                     </div>
-                    <div>Test</div>
-                    <div className='road-range'>
-                        <div>
-                            <span className='yellow'></span>
-                            <span className='yellow-line'></span>
+                    <div className='roads'>
+                        <div className='road-range'>
+                            <div>
+                                <span className='t-text'>November 2017</span>
+                                <span className='yellow'></span>
+                                <span className='yellow-line'></span>
+                                <span className='b-text'>January 2018</span>
+                            </div>
+                            <div>
+                                <span className='yellow'></span>
+                                <span className='yellow-line'></span>
+                                <p style={{ paddingTop: '28px' }}>
+                                    Development of different types of smart
+                                    contracts (CPA, CPL, CPC, CPS, CPI)
+                                </p>
+                            </div>
+
+                            <div>
+                                <span className='t-text'>March 2018</span>
+
+                                <span className='yellow'></span>
+
+                                <span className='yellow-line'></span>
+                                <span className='b-text'>March 2019</span>
+                            </div>
+                            <div>
+                                <span className='black'></span>
+                                <span className='yellow-black'></span>
+                                <p style={{ paddingTop: '28px' }}>
+                                    Release of a decentralized app for merchants
+                                    / affiliates
+                                </p>
+                            </div>
+                            <div>
+                                <span className='t-text'>May 2020</span>
+
+                                <span className='black'></span>
+                                <span className='yellow-black'></span>
+                                <span className='b-text'>September 2020</span>
+                            </div>
+                            <div>
+                                <span className='black'></span>
+                                <span className='yellow-black'></span>
+                                <p style={{ paddingTop: '28px' }}>
+                                    Launch of the world's first decentralized
+                                    affiliate network based on the HOQU platform
+                                </p>
+                            </div>
                         </div>
-                        <div>
-                            <span className='yellow'></span>
-                            <span className='yellow-line'></span>
-                        </div>
-                        <div>
-                            <span className='yellow'></span>
-                            <span className='yellow-line'></span>
-                        </div>
-                        <div>
-                            <span className='yellow'></span>
-                            <span className='yellow-black'></span>
-                        </div>
-                        <div></div>
-                        <div></div>
+                    </div>
+                    <div className='roads-img'>
+                        {roadImages.map((img) => (
+                            <img className='road-img' src={img.img} alt='' />
+                        ))}
                     </div>
                 </div>
             </div>
